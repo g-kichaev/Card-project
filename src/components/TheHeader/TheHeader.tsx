5de829d6c2fd51aa2442d4fee10768214cf8edaa
Georg Kichaev <kichaev111@gmail.com>
2024-07-26 17:23:37 +0300

    first commit

---
+++ src/components/TheHeader/TheHeader.tsx
@@ -0,0 +1,13 @@
+import {ThemeSwitcher} from 'components/ThemeSwitcher'
+
+import styles from './TheHeader.module.scss';
+
+
+export const TheHeader = () => (
+  <div className={styles.theHeader} >
+    <div className={styles.logo}>
+      GITHUB CARD
+    </div>
+    <ThemeSwitcher />
+  </div>
+);
---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "g-kichaev",
+  id: 203239224,
+  avatar: "https://avatars.githubusercontent.com/u/203239224?v=4",
+  name: "Georg",
+  company: "",
+  blog: "https://t.me/g_kichaev",
+  location: "Saint-Petersburg",
+  bio: "Frontend enthusiast developer from SPB, looking for a job",
+  twitter: "",
+  repos: 3,
+  followers: 0,
+  following: 2,
+  created: "2017-03-04T18:02:58Z",
+};
